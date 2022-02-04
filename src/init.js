d1545e1ce69df6d3f806d561ff926b86760f8bd8
treebird <treebird1982@gmail.com>
2022-02-04 22:34:34 +0900

    delete fake Video model:

--- src/init.js
+++ src/init.js
@@ -1,12 +1,8 @@
-import "regenerator-runtime";
-import "dotenv/config";
 import "./db";
 import "./models/Video";
-import "./models/User";
-import "./models/Comment";
 import app from "./server";
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 const handleListening = () =>
   console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
--- src/server.js
+++ src/server.js
@@ -3,9 +3,6 @@ import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-import "./db";
-import "./models/Video";
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -18,7 +15,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
