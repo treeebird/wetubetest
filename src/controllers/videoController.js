da134b870abbeeea53134f9ef9c1c9d48a139898
treebird <treebird1982@gmail.com>
2022-02-06 11:55:41 +0900

    mongoDB save

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,18 @@
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-  return res.render("home", { pageTitle: "Home"});
+const handleSearch = (error, videos) => {
+  console.log("error", error);
+  console.log("videos:", videos);
+}
+
+
+export const home = async (req, res) => {
+  // Video.find({}, handleSearch);
+  console.log("start");
+  const videos = await Video.find({});
+  console.log(videos);
+  console.log("finishied");
+  return res.render("home", { pageTitle: "Home", videos : videos});
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -20,9 +32,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle : "Upload Video"});
 }
 
-export const postUpload = (req, res) => {
-  const { title } = req.body
-  videos.push(newVideo);
-
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body
+    await Video.create({
+    title,
+    description,
+    createAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta : {
+      views: 0,
+      rating: 0
+    }
+  });
   return res.redirect("/");
 }
